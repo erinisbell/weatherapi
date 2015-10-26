8a6af3678c0296d98613d7bde128af370ada4c29
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-26 14:28:34 -0500

    Changed to HTTParty. Still not working

--- main.rb
+++ main.rb
@@ -5,22 +5,19 @@ require 'httparty'
 
 puts "What zipcode would you like to get the weather for?"
 code = gets.chomp.to_i
-puts "What Feature would you like to get for the location"
+puts "What Feature would you like to get for the location?"
+feature = gets.chomp
 
-# feature = {
-#   "astronomy" => "astronomy",
-#   "conditions" => "conditions",
-#   "currenthurricane" => "currenthurricane",
-#   "forecast10day" => "forecast10day",
-# }
+feature = {
+   "Sunrise" => "astronomy",
+   "Current" => "conditions",
+   "Hurricane" => "currenthurricane",
+   "Forecast" => "forecast10day",
+ }
 
 
 
+url = "http://api.wunderground.com/api/4e1235329c36d452/#{feature}/q/#{code}.json"
+result = HTTParty.get(url)
 
-open("http://api.wunderground.com/api/4e1235329c36d452/geolookup/forecast10day/conditions/q/IA/#{code}.json") do |f|
-  json_string = f.read
-  parsed_json = JSON.parse(json_string)
-  location = parsed_json['location']['city']
-  temp_f = parsed_json['current_observation']['temp_f']
-  print "Current temperature in #{location} is: #{temp_f}\n"
-end
+puts result
