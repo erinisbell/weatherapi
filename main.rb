ccc866ca49541d073dacd592000279f01d02bfba
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-26 16:27:43 -0500

    Got hurricanes working

--- main.rb
+++ main.rb
@@ -4,8 +4,8 @@ require 'httparty'
 require 'pry'
  #weather key id: 4e1235329c36d452
 
-puts "What zipcode would you like to get the weather for?"
-code = gets.chomp.to_i
+# puts "What zipcode would you like to get the weather for?"
+# code = gets.chomp.to_i
 # result = "http://api.wunderground.com/api/4e1235329c36d452/forecast10day/q/#{code}.json"
 # url = HTTParty.get(result)
 # 10.times do |i|
@@ -22,23 +22,28 @@ code = gets.chomp.to_i
 # puts "Currently the weather in #{current} is #{temp}"
 
 
-result = "http://api.wunderground.com/api/4e1235329c36d452/astronomy/q/#{code}.json"
-url = HTTParty.get(result)
-sunrise = url['sun_phase']['sunrise']['hour']
-sunrise2 = url['sun_phase']['sunrise']['minute']
-pmtime = url['sun_phase']['sunset']['hour']
-pmtime2 = url['sun_phase']['sunset']['minute']
-#sunrise.each_key {|key, value| puts "The sunrise is at #{key}#{value}"}
-puts "The sunrise is will be at #{sunrise}:#{sunrise2} a.m. and the sunset will be at #{pmtime}:#{pmtime2} p.m."
+# result = "http://api.wunderground.com/api/4e1235329c36d452/astronomy/q/#{code}.json"
+# url = HTTParty.get(result)
+# sunrise = url['sun_phase']['sunrise']['hour']
+# sunrise2 = url['sun_phase']['sunrise']['minute']
+# pmtime = url['sun_phase']['sunset']['hour']
+# pmtime2 = url['sun_phase']['sunset']['minute']
+# puts "The sunrise is will be at #{sunrise}:#{sunrise2} a.m. and the sunset will be at #{pmtime}:#{pmtime2}"
 
 
 # result = "http://api.wunderground.com/api/4e1235329c36d452/astronomy/q/#{code}.json"
 # url = HTTParty.get(result)
-# alerts = url['response']['features']['alerts']
-# puts "Here are the current alerts #{alerts} for #{code}"
+# alerts = url['query_zone']['alerts']
+# puts "Here are the current alerts for #{code}: #{alerts} if empty, there are no alerts for your area. "
 
 
 
+result = "http://api.wunderground.com/api/4e1235329c36d452/currenthurricane/view.json"
+url = HTTParty.get(result)
+number = url['response']['features']['currenthurricane']
+hurricane = url['currenthurricane'][0]['stormInfo']['stormName']
+category = url['currenthurricane'][0]['Current']['Category']
+puts "Here are THE NUMBERS OF HURRICANES ANYWHERE KURT: #{number}. The name of it is: #{hurricane} and it is a #{category}!"
 
 
 
