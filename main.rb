1d92bea4c236517aed67b975e434c6a09c71dc31
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-26 15:22:25 -0500

    Did 10 day forecast

--- main.rb
+++ main.rb
@@ -6,18 +6,25 @@ require 'pry'
 
 puts "What zipcode would you like to get the weather for?"
 code = gets.chomp.to_i
-puts "What Feature would you like to get for the location? (Sunrise, Current, Hurricane, or Forecast)"
-feature = gets.chomp
-
-feature = {
-   "Sunrise" => "astronomy",
-   "Current" => "conditions",
-   "Hurricane" => "currenthurricane",
-   "Forecast" => "forecast10day"
- }
+result = "http://api.wunderground.com/api/4e1235329c36d452/forecast10day/q/#{code}.json"
+url = HTTParty.get(result)
+10.times do |i|
+day = url['forecast']['txt_forecast']['forecastday'][i]['title']
+conditions = url['forecast']['txt_forecast']['forecastday'][i]['fcttext']
+puts "Conditions for #{day}: #{conditions}"
+end
 
 
-url = "http://api.wunderground.com/api/4e1235329c36d452/conditions/q/CA/#{code}.json"
-result = HTTParty.get(url).parse
+result = "http://api.wunderground.com/api/4e1235329c36d452/conditions/q/#{code}.json"
 
-puts result
+
+
+# puts "What Feature would you like to get for the location? (Sunrise, Current, Hurricane, or Forecast)"
+# feature = gets.chomp
+#
+# feature = {
+#    "Sunrise" => "astronomy",
+#    "Current" => "conditions",
+#    "Hurricane" => "currenthurricane",
+#    "Forecast" => "forecast10day"
+#  }
