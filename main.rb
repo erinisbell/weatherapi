96efdf5f42cdba48efb08eb3c6697cd5211e4534
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-26 14:50:35 -0500

    Can get every piece of data for a zipcode but in json

--- main.rb
+++ main.rb
@@ -1,23 +1,23 @@
-require 'open-uri'
+
 require 'json'
 require 'httparty'
+require 'pry'
  #weather key id: 4e1235329c36d452
 
 puts "What zipcode would you like to get the weather for?"
 code = gets.chomp.to_i
-puts "What Feature would you like to get for the location?"
+puts "What Feature would you like to get for the location? (Sunrise, Current, Hurricane, or Forecast)"
 feature = gets.chomp
 
 feature = {
    "Sunrise" => "astronomy",
    "Current" => "conditions",
    "Hurricane" => "currenthurricane",
-   "Forecast" => "forecast10day",
+   "Forecast" => "forecast10day"
  }
 
 
-
-url = "http://api.wunderground.com/api/4e1235329c36d452/#{feature}/q/#{code}.json"
-result = HTTParty.get(url)
+url = "http://api.wunderground.com/api/4e1235329c36d452/conditions/q/CA/#{code}.json"
+result = HTTParty.get(url).parse
 
 puts result
