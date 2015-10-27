f5481a550701b56a563afe07ce94d6a50604ded7
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-27 15:58:30 -0500

    Made it look prettier

--- main.rb
+++ main.rb
@@ -15,20 +15,25 @@ client = Client.new(code)
 current, temp = client.conditions
 puts "Currently the weather in #{current} is #{temp}"
 
+puts "\n"
 sunrise, sunrise2, pmtime, pmtime2 = client.sun
 puts "The sunrise is will be at #{sunrise}:#{sunrise2} a.m. and the sunset will be at #{pmtime}:#{pmtime2}"
+puts "\n"
 
 alerts2 = client.alerts
 puts "Here are the current alerts for #{code}: #{alerts2} if empty, there are no alerts for your area. "
+puts "\n"
 
 hurricanes = client.hurricanes
 puts "Here are the numbers of active hurricanes currently #{hurricanes.count}."
 hurricanes.each do |hurricane|
  puts "The name of it is: #{hurricane[0]} and it is a #{hurricane[1]}!"
 end
+puts "\n"
 
+puts "Here is your ten day forecast for #{code}:"
 str = client.tendayforecast
 puts = "The Ten day Forecast for your area is:"
 str.each do |day, conditions|
-puts " #{day} : #{conditions}" + "\n"
-end 
+puts " * #{day} : #{conditions}" + "\n"
+end
