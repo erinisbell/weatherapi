6d747dad6885a00f299ded51d518198d466edeb5
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-27 13:22:28 -0500

    Updated

--- main.rb
+++ main.rb
@@ -1,49 +1,64 @@
-
+require './weather'
 require 'json'
 require 'HTTParty'
- #weather key id: 4e1235329c36d452
+#weather key id: 4e1235329c36d452
 
 
 
-  #base_uri 'weatherapi.herokuapp.com'
+#base_uri 'weatherapi.herokuapp.com'
 
 puts "What zipcode would you like to get the weather for?"
-code = gets.chomp.to_i
+CODE = gets.chomp.to_i
+KEY = "4e1235329c36d452"
 
-result = "http://api.wunderground.com/api/4e1235329c36d452/forecast10day/q/#{code}.json"
+
+result = "http://api.wunderground.com/api/#{KEY}/forecast10day/q/#{CODE}.json"
 url = HTTParty.get(result)
-10.times do |i|
+20.times do |i|
 day = url['forecast']['txt_forecast']['forecastday'][i]['title']
 conditions = url['forecast']['txt_forecast']['forecastday'][i]['fcttext']
 puts "Conditions for #{day}: #{conditions}"
 end
 
 
-result = "http://api.wunderground.com/api/4e1235329c36d452/geolookup/conditions/q/#{code}.json"
-url = HTTParty.get(result)
-current = url['location']['city']
-temp = url['current_observation']['temp_f']
-puts "Currently the weather in #{current} is #{temp}"
 
-result = "http://api.wunderground.com/api/4e1235329c36d452/astronomy/q/#{code}.json"
-url = HTTParty.get(result)
-sunrise = url['sun_phase']['sunrise']['hour']
-sunrise2 = url['sun_phase']['sunrise']['minute']
-pmtime = url['sun_phase']['sunset']['hour']
-pmtime2 = url['sun_phase']['sunset']['minute']
-puts "The sunrise is will be at #{sunrise}:#{sunrise2} a.m. and the sunset will be at #{pmtime}:#{pmtime2}"
-
-result = "http://api.wunderground.com/api/4e1235329c36d452/astronomy/q/#{code}.json"
-url = HTTParty.get(result)
-alerts = url['query_zone']
-alerts2 = url['alerts']
-puts "Here are the current alerts for #{code}: #{alerts} #{alerts2} if empty, there are no alerts for your area. "
+def conditions
+  result = "http://api.wunderground.com/api/#{KEY}/geolookup/conditions/q/#{CODE}.json"
+  url = HTTParty.get(result)
+  current = url['location']['city']
+  temp = url['current_observation']['temp_f']
+  @conditions = "Currently the weather in #{current} is #{temp}"
+end
+puts conditions
 
 
+def sun
+  result = "http://api.wunderground.com/api/#{KEY}/astronomy/q/#{CODE}.json"
+  url = HTTParty.get(result)
+  sunrise = url['sun_phase']['sunrise']['hour']
+  sunrise2 = url['sun_phase']['sunrise']['minute']
+  pmtime = url['sun_phase']['sunset']['hour']
+  pmtime2 = url['sun_phase']['sunset']['minute']
+  @sun = "The sunrise is will be at #{sunrise}:#{sunrise2} a.m. and the sunset will be at #{pmtime}:#{pmtime2}"
+end
+puts sun
 
-result = "http://api.wunderground.com/api/4e1235329c36d452/currenthurricane/view.json"
-url = HTTParty.get(result)
-number = url['response']['features']['currenthurricane']
-hurricane = url['currenthurricane'][0]['stormInfo']['stormName']
-category = url['currenthurricane'][0]['Current']['Category']
-puts "Here are the numbers of active hurricanes currently #{number}. The name of it is: #{hurricane} and it is a #{category}!"
+def alerts
+  result = "http://api.wunderground.com/api/#{KEY}/alerts/q/#{CODE}.json"
+  url = HTTParty.get(result)
+  alerts = url['query_zone']
+  alerts2 = url['alerts']
+  @alerts = "Here are the current alerts for #{CODE}: #{alerts} #{alerts2} if empty, there are no alerts for your area. "
+end
+puts alerts
+
+
+def hurricane
+  result = "http://api.wunderground.com/api/#{KEY}/currenthurricane/view.json"
+  url = HTTParty.get(result)
+  number = url['response']['features']['currenthurricane']
+  hurricane = url['currenthurricane'][0]['stormInfo']['stormName']
+  category = url['currenthurricane'][0]['Current']['Category']
+  @hurricane = "Here are the numbers of active hurricanes currently #{number}. The name of it is: #{hurricane} and it is a #{category}!"
+end
+puts hurricane
