c7d126192605add0040c7afcc545b17d5fe2ac24
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-27 17:43:30 -0500

    Update test

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,12 +2,23 @@ require 'spec_helper'
 require './client'
 
 RSpec.describe "Client" do
+let(:code) {37206}
 
   describe "#input" do
-    code = 37206
-    client = Client.new(code)
+
+    client = Client.new(:code)
     it "takes a user code" do
       expect(code).to be_integer
     end
   end
+
+
+#   describe "#output" do
+#     output = temperature
+#     client = Client.new(output)
+#     it "outputs json format" do
+#     expect(output).to :format => :json
+#   end
+# end
+
 end
